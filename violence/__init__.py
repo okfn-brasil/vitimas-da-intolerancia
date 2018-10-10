b885ce425dad974e16a522ac468a896f95705734
Eduardo Cuducos <cuducos@gmail.com>
2018-10-10 12:08:15 -0300

    Compress responses

--- violence/__init__.py
+++ violence/__init__.py
@@ -1,11 +1,13 @@
 from sanic import Sanic
+from sanic_compress import Compress
 from sanic_jinja2 import SanicJinja2
 
 from violence.data import Data
 from violence.settings import REFRESH_CACHE_ON_LOAD, STATIC_DIR
 
-app = Sanic()
+app = Sanic('violencia_nao_tem_partido')
 app.static('/static', str(STATIC_DIR))
+Compress(app)
 jinja = SanicJinja2(app, pkg_name='violence')
 app_data = Data(refresh_cache=REFRESH_CACHE_ON_LOAD)
 
