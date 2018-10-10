a60557999e00f46d4992d0423e988e9a112d14d1
Eduardo Cuducos <cuducos@gmail.com>
2018-10-10 12:12:42 -0300

    fixup! Remove stories with missing title

--- violence/__init__.py
+++ violence/__init__.py
@@ -3,7 +3,12 @@ from sanic_compress import Compress
 from sanic_jinja2 import SanicJinja2
 
 from violence.data import Data
-from violence.settings import REFRESH_CACHE_ON_LOAD, STATIC_DIR
+from violence.settings import (
+    REFRESH_CACHE_ON_LOAD,
+    STATIC_DIR,
+    TITLE
+)
+
 
 app = Sanic('violencia_nao_tem_partido')
 app.static('/static', str(STATIC_DIR))
@@ -16,3 +21,4 @@ app_data = Data(refresh_cache=REFRESH_CACHE_ON_LOAD)
 @jinja.template('index.html')
 async def index(request):
     return {'cases': app_data.cases}
+    return {'cases': app_data.cases, 'title': TITLE}
