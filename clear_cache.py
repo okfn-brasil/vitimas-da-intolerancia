c9de33a132803a936c18cf93a56387b79b57e97e
Eduardo Cuducos <cuducos@gmail.com>
2018-10-10 19:21:31 -0300

    Add success message to clear cache script

--- clear_cache.py
+++ clear_cache.py
@@ -3,4 +3,8 @@ from redis import StrictRedis
 from victims.settings import REDIS_DB, REDIS_URL
 
 
-StrictRedis.from_url(REDIS_URL, db=REDIS_DB).flushall()
+cache = StrictRedis.from_url(REDIS_URL, db=REDIS_DB)
+total = len(cache.keys())
+print(f"Flushing {total} key/value pair(s)")
+cache.flushall()
+print("Done :)")
