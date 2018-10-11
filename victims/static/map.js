92a12770a10c6233bc6cf92803071cc23d463dfb
Ellison Leão <ellisonleao@gmail.com>
2018-10-11 17:01:02 -0300

    limit map bounds to brazil

--- victims/static/map.js
+++ victims/static/map.js
@@ -4,8 +4,17 @@ $(document).ready(function () {
 
     L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-        maxZoom: 13
+        maxZoom: 13,
+        minZoom: 4
     }).addTo(map);
+
+    // fit bounds to brazil country only
+    var limit = new L.Polyline([
+        [3.843213, -51.835080], // oiapoque
+        [-33.689524, -53.454704] // chuí
+    ]);
+    var bounds = limit.getBounds();
+    map.fitBounds(bounds).setMaxBounds(bounds);
 
     // TODO: Create markers and popups
 });
