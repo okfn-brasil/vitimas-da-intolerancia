de4cab5e6f3d6abd0173b8a374e39782c56d8ae0
Eduardo Cuducos <cuducos@users.noreply.github.com>
2019-06-18 14:52:26 -0300

    Remove HTTPS middleware
    This might sound counterintuitive but this commit is part of a
    overarching change in this project, aiming at transforming it in a
    static generator instead of a conventional web app.

--- victims/__init__.py
+++ victims/__init__.py
@@ -1,32 +1,15 @@
-from urllib.parse import urlunparse
-
 from sanic import Sanic
-from sanic.response import redirect
 from sanic_compress import Compress
 from sanic_jinja2 import SanicJinja2
 
 from victims.data import Data
-from victims.settings import CASE_MAX_CHARS, DEBUG, STATIC_DIR, TITLE
+from victims.settings import CASE_MAX_CHARS, STATIC_DIR, TITLE
 
 
 app = Sanic("vitimas_da_intolerancia")
 app.static("/static", str(STATIC_DIR))
 jinja = SanicJinja2(app, pkg_name="victims")
 Compress(app)
-
-
-@app.middleware("request")
-def force_https(request):
-    if DEBUG:
-        return None
-
-    host = request.headers.get("Host", "")
-    protocol = "https" if request.transport.get_extra_info("sslcontext") else "http"
-
-    if request.headers.get("x-forwarded-proto", protocol) == "http":
-        args = ("https", host, request.path, None, request.query_string, None)
-        url = urlunparse(args)
-        return redirect(url)
 
 
 @app.route("/")
